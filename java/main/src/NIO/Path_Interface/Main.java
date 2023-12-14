e117e2f83464680777c787712149cf1f24bbaf5b
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-12-14 11:34:21 +0100

    Path commit

--- java/main/src/NIO/Path_Interface/Main.java
+++ java/main/src/NIO/Path_Interface/Main.java
@@ -1,9 +1,30 @@
 package NIO.Path_Interface;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+  public static void main(String[] args) throws IOException {
 
-  public static void main(String[] args) {
+    // absolute path is like :
+    // '/Users/safwankherallah/Documents/DCI_Java/Java_Files_and_IO/java/main/resources/movies.txt'
 
 
+    String fileName = "java/main/resources/movies.txt";
+    Path filePath = Paths.get(fileName);
+    System.out.println("Path '"+filePath+"' is "+ (filePath.isAbsolute() ? "absolute" : "relative"));
+    try {
+      Path realFilePath = filePath.toRealPath();
+      System.out.println("Path '"+realFilePath+"' is "+ (realFilePath.isAbsolute() ? "absolute" : "relative"));
+    }catch (IOException e){
+      e.printStackTrace(System.err);
+    }
+
+    /**
+     * Output:
+     * Path 'java/main/resources/movies.txt' is relative
+     * Path '/Users/safwankherallah/Documents/DCI_Java/Java_Files_and_IO/java/main/resources/movies.txt' is absolute
+     * */
   }
 }
